ef399c37595148071525fade8ddef75a739213fa
Andrew Zito <zitoa@lafayette.edu>
2019-02-04 12:46:57 -0500

    Remember shouldn't require id

--- bin/remember.py
+++ bin/remember.py
@@ -7,7 +7,7 @@ M = Mind()
 
 if __name__ == '__main__':
     text = sys.argv[1]
-    id = sys.argv[2]
+    id = sys.argv[2] if len(sys.argv) > 2 else None
     mem = Memory.from_text(None, text)
     mem.id = id
     M.remember(mem)
